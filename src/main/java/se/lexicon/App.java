05499fe5a3e87ecb84a87052cc22db215e3dad28
Mykyta Shadrin <nikitashadriiiin@gmail.com>
2025-06-27 09:35:30 +0200

    done

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -9,7 +9,15 @@ public class App {
         NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan"});
         System.out.println(NameRepository.getSize());
         // call more methods as needed
-        NameRepository.clear();
+        //NameRepository.clear();
+        String[] newArray = NameRepository.findAll();
+        System.out.println(newArray.length);
+        System.out.println(NameRepository.find("Erik Svensson"));
+        NameRepository.add("Mykyta Shadrin");
         System.out.println(NameRepository.getSize());
+        NameRepository.findByFirstName("Maksim");
+        newArray = NameRepository.findByLastName("Shadrin");
+        System.out.println(newArray.length);
+
     }
 }
--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -1,6 +1,5 @@
 package se.lexicon;
 
-import java.sql.Array;
 import java.util.Arrays;
 
 /**
@@ -18,7 +17,6 @@ public class NameRepository {
      * @return The number of elements in the names array.
      */
     public static int getSize() {
-        //todo: implement getSize method
         return names.length;
     }
 
@@ -29,7 +27,6 @@ public class NameRepository {
      * @param names The array of names to set.
      */
     public static void setNames(String[] newNames) {
-        //todo: implement setNames method
         names = newNames;
     }
 
@@ -38,7 +35,6 @@ public class NameRepository {
      * Clears the names array by creating a new empty array.
      */
     public static void clear() {
-        //todo: implement clear method
         names = new String[0];
     }
 
@@ -49,7 +45,6 @@ public class NameRepository {
      * @return A new array containing all elements from the names array.
      */
     public static String[] findAll() {
-        //todo: implement findAll method
         String[] newArray = new String[names.length];
         System.arraycopy(names, 0, newArray, 0, names.length);
         return newArray;
@@ -63,8 +58,6 @@ public class NameRepository {
      * @return The matching name if found; otherwise, null.
      */
     public static String find(String fullName) {
-        //todo: implement find method
-        //return null;
         String[] newArray = Arrays.copyOf(names, names.length);
         Arrays.sort(newArray, String.CASE_INSENSITIVE_ORDER);
         int indexFound = Arrays.binarySearch(newArray, fullName);
@@ -86,7 +79,6 @@ public class NameRepository {
      * @return True if the fullName is added successfully; false if it already exists.
      */
     public static boolean add(String fullName) {
-        //todo: implement add method
         String findingResult = find(fullName);
         if(findingResult != null){
             System.out.println("This name already exists");
@@ -108,8 +100,26 @@ public class NameRepository {
      * @return An array containing all matching names.
      */
     public static String[] findByFirstName(String firstName) {
-        //todo: findByFirstName method
-        return null;
+        int count = 0;
+        for (String name : names) {
+            if (name.startsWith(firstName + " ")) {
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("There is no such person with that name here: " + firstName);
+            return new String[0];
+        } else{
+            String[] result = new String[count];
+            int index = 0;
+            for (String name : names) {
+                if (name.startsWith(firstName + " ")) {
+                    result[index++] = name;
+                }
+            }
+            return result;
+        }
     }
 
 
@@ -120,8 +130,26 @@ public class NameRepository {
      * @return An array containing all matching names.
      */
     public static String[] findByLastName(String lastName) {
-        //todo: implement findByLastName method
-        return null;
+        int count = 0;
+        for (String name : names) {
+            if (name.endsWith(" " + lastName)) {
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("There is no such person with that lastname here: " + lastName);
+            return new String[0];
+        } else{
+            String[] result = new String[count];
+            int index = 0;
+            for (String name : names) {
+                if (name.endsWith(" " + lastName)) {
+                    result[index++] = name;
+                }
+            }
+            return result;
+        }
     }
 
 
@@ -133,8 +161,20 @@ public class NameRepository {
      * @return True if the name is updated successfully; false if the updated name already exists or the original name is not found.
      */
     public static boolean update(String original, String updatedName) {
-        //todo: implement update method
-        return false;
+        String[] newArray = Arrays.copyOf(names, names.length);
+        Arrays.sort(newArray, String.CASE_INSENSITIVE_ORDER);
+        int originalFound = Arrays.binarySearch(newArray, original);
+        int updatedFound = Arrays.binarySearch(newArray, updatedName);
+        if(updatedFound >= 0 ){
+            System.out.println("The updated name already exists");
+            return false;
+        } else if (originalFound < 0) {
+            System.out.println("There is no such person here: " + original);
+            return false;
+        }else {
+            names[originalFound] = updatedName;
+            return true;
+        }
     }
 
 
@@ -145,9 +185,27 @@ public class NameRepository {
      * @return True if the name is removed successfully; false if the name is not found in the array.
      */
     public static boolean remove(String fullName) {
-        //todo: implement remove method
-        return false;
+         int indexToRemove = -1;
+            for (int i = 0; i < names.length; i++) {
+                if (names[i].equals(fullName)) {
+                    indexToRemove = i;
+                    break;
+                }
+            }
+
+            if (indexToRemove < 0) {
+                System.out.println("There is no such person here: " + fullName);
+                return false;
+            }
+
+            String[] newArray = new String[names.length - 1];
+
+            for (int i = 0, j = 0; i < names.length; i++) {
+                if (i != indexToRemove) {
+                    newArray[j++] = names[i];
+                }
+            }
+            names = newArray;
+            return true;
     }
-
-
 }
